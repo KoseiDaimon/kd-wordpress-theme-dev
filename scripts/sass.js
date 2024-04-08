130ba5361795d345f80829cd484178537ad41c68
KoseiDaimon <kosei.daimon@gmail.com>
2024-04-09 00:15:43 +0900

    scss複数コンパイル可

--- scripts/sass.js
+++ scripts/sass.js
@@ -6,37 +6,44 @@ import { glob } from "glob";
 const currentDir = process.cwd();
 console.log(`Current directory: ${currentDir}`);
 
-const scssFilePath = path.join(currentDir, "src", "scss", "common", "common.scss");
-const cssOutputDir = path.join(currentDir, "assets", "css");
-const cssOutputPath = path.join(cssOutputDir, "common.css");
+// const scssFilePath = path.join(currentDir, "src", "scss", "common", "common.scss");
+// const cssOutputDir = path.join(currentDir, "assets", "css");
+// const cssOutputPath = path.join(cssOutputDir, "common.css");
 
-try {
-  const result = await sass.compileAsync(scssFilePath);
-  await fs.mkdir(cssOutputDir, { recursive: true });
-  await fs.writeFile(cssOutputPath, result.css);
-  console.log(`${scssFilePath} has been compiled to ${cssOutputPath}`);
-} catch (err) {
-  console.error(`Failed to compile ${scssFilePath}: ${err}`);
-}
-
-// const scssGlob = "./src/scss/**/*.scss";
-// const cssOutputDir = "./assets/css";
-
-// const srcPaths = await glob(scssGlob);
-// console.log(srcPaths);
-
-// for (const srcPath of srcPaths) {
-//   const distPath = `${cssOutputDir}/${path.basename(srcPath, ".scss")}.css`;
-//   console.log(distPath);
-
-//   try {
-//     const result = await sass.compileAsync(srcPath);
-//     await fs.writeFile(srcPath, result.css);
-//     console.log(`${srcPath} has been compiled to ${srcPath}`);
-//   } catch (err) {
-//     console.error(`Failed to compile ${srcPath}: ${err}`);
-//   }
+// try {
+//   const result = await sass.compileAsync(scssFilePath);
+//   await fs.mkdir(cssOutputDir, { recursive: true });
+//   await fs.writeFile(cssOutputPath, result.css);
+//   console.log(`${scssFilePath} has been compiled to ${cssOutputPath}`);
+// } catch (err) {
+//   console.error(`Failed to compile ${scssFilePath}: ${err}`);
 // }
+
+const scssGlob = "./src/scss/**/*.scss";
+const distDir = "./assets/css";
+
+const srcPaths = await glob(scssGlob);
+console.log(srcPaths);
+
+await fs.mkdir(distDir, { recursive: true });
+
+for (const srcPath of srcPaths) {
+  if (path.basename(srcPath).startsWith("_")) {
+    continue;
+  }
+
+  const distFileName = path.basename(srcPath, ".scss") + ".css";
+  const distPath = path.join(distDir, distFileName);
+  console.log(distPath);
+
+  try {
+    const result = await sass.compileAsync(srcPath);
+    await fs.writeFile(distPath, result.css);
+    console.log(`${srcPath} has been compiled to ${distPath}`);
+  } catch (err) {
+    console.error(`Failed to compile ${srcPath}: ${err}`);
+  }
+}
 
 // glob(scssGlob, {}, async (err, scssFilePaths) => {
 //   if (err) {
