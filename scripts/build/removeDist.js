ccbf9163cf8d3b48fa575dd630c4fad4b93d0bcb
KoseiDaimon <kosei.daimon@gmail.com>
2024-04-11 16:40:15 +0900

    config.distを個別に削除するように変更

--- scripts/build/removeDist.js
+++ scripts/build/removeDist.js
@@ -3,22 +3,24 @@ import { config } from "../../config.js";
 import Logger from "../utils/Logger.js";
 
 // 削除対象のディレクトリを指定
-const distDir = "./assets";
+const distDirs = Object.values(config.dist);
 
-try {
-  // ディレクトリを再帰的に削除
-  await rm(distDir, { recursive: true });
-  // 削除成功のメッセージを表示
-  Logger.log("INFO", `Removed directory ${distDir}`);
-} catch (err) {
-  // エラーが発生した場合の処理
-  if (err.code === "ENOENT") {
-    // ディレクトリが存在しない場合のメッセージを表示
-    Logger.log("DEBUG", `Directory ${distDir} does not exist`);
-  } else {
-    // その他のエラーが発生した場合のメッセージを表示
-    Logger.log("ERROR", `Error removing directory ${distDir}`);
-    // エラーの詳細を表示
-    Logger.log("ERROR", err);
+for (const distDir of distDirs) {
+  try {
+    // ディレクトリを再帰的に削除
+    await rm(distDir, { recursive: true });
+    // 削除成功のメッセージを表示
+    Logger.log("INFO", `Removed directory ${distDir}`);
+  } catch (err) {
+    // エラーが発生した場合の処理
+    if (err.code === "ENOENT") {
+      // ディレクトリが存在しない場合のメッセージを表示
+      Logger.log("DEBUG", `Directory ${distDir} does not exist`);
+    } else {
+      // その他のエラーが発生した場合のメッセージを表示
+      Logger.log("ERROR", `Error removing directory ${distDir}`);
+      // エラーの詳細を表示
+      Logger.log("ERROR", err);
+    }
   }
 }
