32528d0f8e4d7c4661656fc4bde966c3c82f5573
KoseiDaimon <kosei.daimon@gmail.com>
2024-04-11 16:19:50 +0900

    createIndexContentをプライベートメソッドに変更

--- scripts/utils/ScssProcessor.js
+++ scripts/utils/ScssProcessor.js
@@ -3,17 +3,7 @@ import path from "path";
 import fs from "fs/promises";
 import { glob } from "glob";
 import { config } from "../../config.js";
-import Logger from "./Logger.js";
-
-// インデックスファイルの内容を生成する関数
-const createIndexContent = (scssFiles) => {
-  // インデックスファイルの内容を配列で作成
-  return [
-    "// This file is generated automatically by scripts/generateIndexFiles.js",
-    // 各 SCSS パーシャルファイルを @forward ディレクティブでインポート
-    ...scssFiles.map((file) => `@forward "./${file.slice(0, -5)}";`),
-  ].join("\n"); // 配列の要素を改行で結合して文字列に変換
-};
+import Logger from "../utils/Logger.js";
 
 // SCSS プロセッサクラス
 export default class ScssProcessor {
@@ -21,52 +11,45 @@ export default class ScssProcessor {
     this.srcDir = srcDir;
     this.distDir = distDir;
   }
-  // インデックスファイルを生成するメソッド
+
+  #createIndexContent(scssFiles) {
+    return [
+      "// This file is generated automatically by scripts/generateIndexFiles.js",
+      ...scssFiles.map((file) => `@forward "./${file.slice(0, -5)}";`),
+    ].join("\n");
+  }
+
   async generateIndexFiles(dir = this.srcDir) {
     try {
-      // ディレクトリ内のファイル一覧を取得
       const files = await fs.readdir(dir);
-      // SCSS パーシャルファイルのみをフィルタリング
       const scssFiles = files.filter(
         (file) => file.endsWith(".scss") && file.startsWith("_") && file !== "_index.scss"
       );
 
-      // ディレクトリ内の各ファイルに対して処理を行う
       for (const file of files) {
-        // ファイルの絶対パスを作成
         const filePath = path.join(dir, file);
-        // ファイルの情報を取得
         const stat = await fs.stat(filePath);
-        // ファイルがディレクトリの場合、再帰的に generateIndexFiles メソッドを呼び出す
         if (stat.isDirectory()) {
           await this.generateIndexFiles(filePath);
         }
       }
 
-      // SCSS パーシャルファイルが存在しない場合、処理を終了
       if (scssFiles.length === 0) {
         return;
       }
 
-      // インデックスファイルの絶対パスを作成
       const indexFilePath = path.join(dir, "_index.scss");
-      // インデックスファイルの内容を生成
-      const indexContent = createIndexContent(scssFiles);
+      const indexContent = this.#createIndexContent(scssFiles);
 
       try {
-        // インデックスファイルが既に存在するかどうかを確認
         await fs.access(indexFilePath);
-        // 既に存在する場合、上書きする旨のメッセージを表示
         Logger.log("INFO", `Overwriting existing ${indexFilePath}`);
       } catch {
-        // 存在しない場合、新規作成する旨のメッセージを表示
         Logger.log("INFO", `Generating new ${indexFilePath}`);
       }
 
-      // インデックスファイルを書き込む
       await fs.writeFile(indexFilePath, indexContent);
     } catch (error) {
-      // エラーが発生した場合、エラーメッセージを表示
       Logger.log("ERROR", `Error generating index file in ${dir}`);
       Logger.log("ERROR", error);
     }
